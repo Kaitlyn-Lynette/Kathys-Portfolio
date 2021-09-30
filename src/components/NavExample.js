b2ef700e3b5b7d7f6f39e8b6a53da969a00791ff
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-09-30 13:35:30 -0400

    Styling navbar

--- src/components/NavExample.js
+++ src/components/NavExample.js
@@ -1,8 +1,5 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import "./NavExample.css";
 
 const NavExample = () => {
