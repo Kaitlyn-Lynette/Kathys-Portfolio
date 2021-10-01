b314e048b768dcbae5a7208a8781447b6cdaa381
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-01 11:30:58 -0400

    Created a new page and splash image component

---
+++ src/components/SplashImage.js
@@ -0,0 +1,12 @@
+import Image from "react-bootstrap/Image";
+
+
+const SplashImage = () => {
+    return (
+        <div>
+            <Image src="../images/KathySplash1472x595.png" width="100%" fluid />
+        </div>
+    )
+}
+
+export default SplashImage
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavExample from "../components/NavExample";
+import SplashImage from "../components/SplashImage";
+
+
+const Home = () => {
+    return (
+        <div>
+            <NavExample />
+            <SplashImage />
+        </div>
+    )
+}
+
+export default Home
