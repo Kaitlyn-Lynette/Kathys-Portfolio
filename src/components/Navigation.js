4db508d799243b63639da48ea3a7a653504a516e
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-04 13:01:53 -0400

    Added a drop down

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Image from "react-bootstrap/Image";
 import "./Navigation.css";
 
@@ -16,7 +17,14 @@ const Navigation = () => {
       <Image src="/images/Kathy_Logo_500x100px-transparent.png" className="nav-brand" />
       <Nav className="nav-links">
         <Nav.Link className="nav-item" href="#about">{navOne}</Nav.Link>
-        <Nav.Link className="nav-item" href="#work">{navTwo}</Nav.Link>
+        {/* <Nav.Link className="nav-item" href="#work">{navTwo}</Nav.Link> */}
+      <NavDropdown className="nav-item" title={navTwo} id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#work">Book Edits</NavDropdown.Item> 
+        <NavDropdown.Item href="#work">Paid Writing</NavDropdown.Item>
+        <NavDropdown.Item href="#work">My Personal Writing</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#work">Read with Coffee</NavDropdown.Item>
+      </NavDropdown>
         <Nav.Link className="nav-item" href="#services">{navThree}</Nav.Link>
         <Nav.Link className="nav-item" href="#testimonials">{navFour}</Nav.Link>
         <Nav.Link className="nav-item" href="#contact">{navFive}</Nav.Link>
