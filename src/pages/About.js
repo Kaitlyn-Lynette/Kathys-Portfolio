00126e0caf0d8d86e1cc49e5576c5bd131562d4d
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-07 13:32:26 -0400

    Added the formatting for the About section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image"
+import "../App.css"
 
 const About = () => {
   return (
@@ -8,17 +10,17 @@ const About = () => {
       <Container>
         <Row>
           <Col md={4}>
-            <h2>About my story</h2>
-            <h3>
-              Katherine is an editor, writer, cultural curator based in
+            <p>About my story</p>
+            <h4>
+              Katherine is an editor, writer, and mentor based in
               Brooklyn, NY
-            </h3>
+            </h4>
             <p>
               Katherine graduated from xxx University, with a Bachelor in X, Y,
               Z. She has edited books for social leaders in Sunset Park.
             </p>
           </Col>
-          <Col md={4}>Picture</Col>
+          <Col md={4}><Image src="/images/AboutProfile345x400.png" width="100%"></Image></Col>
           <Col md={4}>
             <h3>Note to my readers</h3>
             <p>
