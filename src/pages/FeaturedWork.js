256f0ca020c2a0316e3d391592591e75babc4cc9
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-23 21:31:34 -0400

    Added Copywriting Section

--- src/pages/FeaturedWork.js
+++ src/pages/FeaturedWork.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
 import SideBarNav from "../components/SideBarNav";
 import BookEdits from "../pages/BookEdits";
 import FeaturedHeader from "../components/FeaturedHeader";
+import CopyWriting from "../pages/CopyWriting"
 
 const FeaturedWork = () => {
   return (
@@ -13,6 +10,7 @@ const FeaturedWork = () => {
       <SideBarNav></SideBarNav>
       <FeaturedHeader></FeaturedHeader>
       <BookEdits></BookEdits>
+      <CopyWriting></CopyWriting>
     </div>
   );
 };
