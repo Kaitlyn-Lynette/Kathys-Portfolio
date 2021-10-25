c5387f6269d9877bfad17969a3e271d64de143b9
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-25 14:30:03 -0400

    Added styling to css and added filter on hover

--- src/pages/PersonalWriting.js
+++ src/pages/PersonalWriting.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Card from "react-bootstrap/Card";
+import "../App.css";
 
 const PersonalWriting = () => {
+  const [isShown, setIsShown] = useState(false);
   return (
     <div id="personalwriting">
       <Container>
@@ -16,16 +18,12 @@ const PersonalWriting = () => {
         </Row>
         <Row style={{ paddingTop: "80px" }}>
           <Col md={{ span: 6, offset: 4 }}>
-            <Card className="bg-dark text-white">
-              <Card.Img src="/images/blog_home.png" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title>Home</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
+            <Card
+              onMouseEnter={() => setIsShown(!isShown)}
+              >
+              <Image src="/images/blog_home.png" alt="homeblogfeature" />
+              <Card.ImgOverlay style={{ lineHeight: "300px" }}>
+                <Card.Title className="blogHeader">Home</Card.Title>
               </Card.ImgOverlay>
             </Card>
             <div>
@@ -34,13 +32,13 @@ const PersonalWriting = () => {
                   http://www.tomyunderstanding.net/2019/12/people-of-intentional-connection.html
                 </li>
                 <li>
-                http://www.tomyunderstanding.net/2018/01/my-cozy-place.html
+                  http://www.tomyunderstanding.net/2018/01/my-cozy-place.html
                 </li>
                 <li>
-                http://www.tomyunderstanding.net/2017/05/awakening-spring-cleaning-is-actually.html
+                  http://www.tomyunderstanding.net/2017/05/awakening-spring-cleaning-is-actually.html
                 </li>
                 <li>
-                http://www.tomyunderstanding.net/2016/06/stepping-into-marriage.html
+                  http://www.tomyunderstanding.net/2016/06/stepping-into-marriage.html
                 </li>
               </ul>
             </div>
@@ -48,25 +46,21 @@ const PersonalWriting = () => {
         </Row>
         <Row style={{ paddingTop: "20px" }}>
           <Col md={{ span: 6, offset: 4 }}>
-            <Card className="bg-dark text-white">
-              <Card.Img src="/images/blog_2.png" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title>Creativity & Passion</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
+            <Card className="text-white rounded-0" style={{ border: "none" }}>
+              <Image src="/images/blog_2.png" alt="Card image" />
+              <Card.ImgOverlay style={{ lineHeight: "300px" }}>
+                <Card.Title className="blogHeader">
+                  Creativity & Passion
+                </Card.Title>
               </Card.ImgOverlay>
             </Card>
             <div>
               <ul>
                 <li>
-                http://www.tomyunderstanding.net/2019/07/stalling-our-creativity.html
+                  http://www.tomyunderstanding.net/2019/07/stalling-our-creativity.html
                 </li>
                 <li>
-                http://www.tomyunderstanding.net/2018/02/where-to-find-your-passion.html
+                  http://www.tomyunderstanding.net/2018/02/where-to-find-your-passion.html
                 </li>
               </ul>
             </div>
@@ -74,18 +68,30 @@ const PersonalWriting = () => {
         </Row>
         <Row style={{ paddingTop: "20px" }}>
           <Col md={{ span: 6, offset: 4 }}>
-            <Card className="bg-dark text-white">
-              <Card.Img src="/images/blog_3.png" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title>Wisdom & Womanhood</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
+            <Card className="text-white rounded-0" style={{ border: "none" }}>
+              <Image src="/images/blog_3.png" alt="Card image" />
+              <Card.ImgOverlay className="blogImgCards">
+                <Card.Title className="blogHeader">
+                  Wisdom & Womanhood
+                </Card.Title>
               </Card.ImgOverlay>
             </Card>
+            <div>
+              <ul>
+                <li>
+                  http://www.tomyunderstanding.net/2020/12/the-discomfort-and-peace-of-being.html
+                </li>
+                <li>
+                  http://www.tomyunderstanding.net/2021/09/inspire-series-showing-grace-to-dream.html
+                </li>
+                <li>
+                  http://www.tomyunderstanding.net/2020/06/when-comparison-leads-to-envy.html
+                </li>
+                <li>
+                  http://www.tomyunderstanding.net/2019/06/before-you-follow-that-trend.html
+                </li>
+              </ul>
+            </div>
           </Col>
         </Row>
       </Container>
