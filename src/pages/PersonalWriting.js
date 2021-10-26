fd78a38480d4693ecc704bb603ed0d1c333b8e30
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-26 09:34:58 -0400

    Styling to personalwriting section

--- src/pages/PersonalWriting.js
+++ src/pages/PersonalWriting.js
@@ -9,65 +9,54 @@ import "../App.css";
 const PersonalWriting = () => {
   const [isShown, setIsShown] = useState(false);
   return (
-    <div id="personalwriting">
+    <div id="personal" 
+    style= {{paddingTop: "80px",
+    paddingBottom: "100px"}}>
       <Container>
         <Row>
-          <Col style={{ paddingTop: "100px" }} md={{ span: 6, offset: 6 }}>
+          <Col md={{ span: 4, offset: 5 }}>
             <h1>Personal Writing</h1>
           </Col>
         </Row>
         <Row style={{ paddingTop: "80px" }}>
-          <Col md={{ span: 6, offset: 4 }}>
-            <Card
-              onMouseEnter={() => setIsShown(!isShown)}
-              >
+          <Col md={4}>
+            <Card style={{ border: "none" }} onMouseEnter={() => setIsShown(!isShown)}>
               <Image src="/images/blog_home.png" alt="homeblogfeature" />
-              <Card.ImgOverlay style={{ lineHeight: "300px" }}>
+              <Card.ImgOverlay>
                 <Card.Title className="blogHeader">Home</Card.Title>
               </Card.ImgOverlay>
             </Card>
-            <div>
+            <div className="blog-list">
               <ul>
-                <li>
-                  http://www.tomyunderstanding.net/2019/12/people-of-intentional-connection.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2018/01/my-cozy-place.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2017/05/awakening-spring-cleaning-is-actually.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2016/06/stepping-into-marriage.html
-                </li>
+                <li>People of Intentional Connection, Dec 2019</li>
+                <li>My Cozy Place, Jan 2018</li>
+                <li>Stepping Into Marriage, Jul 2016</li>
               </ul>
             </div>
           </Col>
-        </Row>
-        <Row style={{ paddingTop: "20px" }}>
-          <Col md={{ span: 6, offset: 4 }}>
+
+          <Col md={4}>
             <Card className="text-white rounded-0" style={{ border: "none" }}>
-              <Image className="img-port" src="/images/blog_2.png" alt="Card image" />
-              <Card.ImgOverlay style={{ lineHeight: "300px"}}>
+              <Image
+                className="img-port"
+                src="/images/blog_2.png"
+                alt="Card image"
+              />
+              <Card.ImgOverlay>
                 <Card.Title className="blogHeader">
                   Creativity & Passion
                 </Card.Title>
               </Card.ImgOverlay>
             </Card>
-            <div>
+            <div className="blog-list">
               <ul>
-                <li>
-                  http://www.tomyunderstanding.net/2019/07/stalling-our-creativity.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2018/02/where-to-find-your-passion.html
-                </li>
+                <li>Stalling Our Creativity, July 2019</li>
+                <li>Where to Find Your Passion, July 2018</li>
               </ul>
             </div>
           </Col>
-        </Row>
-        <Row style={{ paddingTop: "20px" }}>
-          <Col md={{ span: 6, offset: 4 }}>
+
+          <Col md={4}>
             <Card className="text-white rounded-0" style={{ border: "none" }}>
               <Image src="/images/blog_3.png" alt="Card image" />
               <Card.ImgOverlay className="blogImgCards">
@@ -76,20 +65,12 @@ const PersonalWriting = () => {
                 </Card.Title>
               </Card.ImgOverlay>
             </Card>
-            <div>
+            <div className="blog-list">
               <ul>
-                <li>
-                  http://www.tomyunderstanding.net/2020/12/the-discomfort-and-peace-of-being.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2021/09/inspire-series-showing-grace-to-dream.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2020/06/when-comparison-leads-to-envy.html
-                </li>
-                <li>
-                  http://www.tomyunderstanding.net/2019/06/before-you-follow-that-trend.html
-                </li>
+                <li>Discomfort and Peace of Being, Dec 2020</li>
+                <li>Inspire Series: Showing the Grace to Dream, Sept 2021</li>
+                <li>When Comparison Leads to Envy, June 2020</li>
+                <li>Before You Follow That Trend, June 2019</li>
               </ul>
             </div>
           </Col>
