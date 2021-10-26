d28414b89cf8bb1e8206962c0a96dd125c6707c6
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-26 08:52:43 -0400

    styling for copywriting section

--- src/pages/CopyWriting.js
+++ src/pages/CopyWriting.js
@@ -1,37 +1,51 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 
 const CopyWriting = () => {
-    return (
-        <div id="copywriting">
-            <Container style={{backgroundColor: "#E5B87D"}}>
-            <Row>
-                <Col style={{paddingTop: "100px"}} md={{ span: 6, offset: 6 }}>
-                <h1>Copy Writing</h1>
-                </Col>
-            </Row>
-            <Row style={{paddingTop: "80px"}}>
-                <Col md={{span: 3, offset: 3 }}>
-                <Image src="/images/CopyWritingExample.png"></Image>
-                </Col>
-                <Col md={{span:3, offset: 1}}>
-                <h4>Blend: Coffee + CoWork</h4>
-                </Col>
-            </Row>
-            <Row style={{paddingTop: "80px"}}>
-                <Col md={{span: 3, offset: 3 }}>
-                <Image src="/images/CopyWritingExample.png"></Image>
-                </Col>
-                <Col md={{span:3, offset: 1}}>
-                <h4>Example #2</h4>
-                </Col>
-            </Row>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div
+      id="copywriting"
+      style={{
+        backgroundColor: "#b6ccc8",
+        paddingTop: "80px",
+        paddingBottom: "100px",
+      }}
+    >
+      {/* <div id="copyTitle" style={{ paddingBottom: "50px" }}>
+        <Row>
+          <Col md={{ span: 2, offset: 2 }}>
+            <h1>Copy Writing </h1>
+          </Col>
+        </Row>
+      </div> */}
+      <Container>
+        <Row>
+          <Col md={4} style={{paddingBottom: "70px" }}>
+            <h1>Copy Writing </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={2}>
+            <Image src="/images/CopyWritingExample.png"></Image>
+          </Col>
+          <Col md={{ span: 3, offset: 3 }}>
+            <h4>Blend: Coffee + CoWork</h4>
+          </Col>
+        </Row>
+        <Row style={{ paddingTop: "80px" }}>
+          <Col md={2}>
+            <Image src="/images/CopyWritingExample.png"></Image>
+          </Col>
+          <Col md={{ span: 3, offset: 3 }}>
+            <h4>Example #2</h4>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default CopyWriting
+export default CopyWriting;
