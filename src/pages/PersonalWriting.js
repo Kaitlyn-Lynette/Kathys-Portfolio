ff0909323c4e386e3d252b631529aa00aafd71e9
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-10-27 23:13:28 -0400

    Edits to personal writing for laptop

--- src/pages/PersonalWriting.js
+++ src/pages/PersonalWriting.js
@@ -9,26 +9,28 @@ import "../App.css";
 const PersonalWriting = () => {
   const [isShown, setIsShown] = useState(false);
   return (
-    <div id="personal" 
-    style= {{paddingTop: "80px",
-    paddingBottom: "100px"}}>
+    <div id="personal" style={{ paddingTop: "80px", paddingBottom: "100px" }}>
       <Container>
         <Row>
-          <Col md={{ span: 4, offset: 6}}  style={{ paddingBottom: "40px" }}>
+          <Col md={{ span: 4, offset: 5 }} style={{ paddingBottom: "40px" }}>
             <h1>Personal Writing</h1>
           </Col>
-          </Row>
-          <Row>
-          <Col md={{ span: 4, offset: 6}} style={{textAlign: "center"}}>
+        </Row>
+        <Row>
+          <Col md={{ span: 5, offset: 4 }} style={{ textAlign: "center" }}>
             <p>
-            From blog posts to featured pieces, links to my personal writing can be found here.
+              From blog posts to featured pieces, links to my personal writing
+              can be found here.
             </p>
           </Col>
         </Row>
         <Row style={{ paddingTop: "80px" }}>
-          <Col md={4}>
-            <Card style={{ border: "none"}} onMouseEnter={() => setIsShown(!isShown)}>
-              <Image src="/images/blog_home.png" alt="homeblogfeature" fluid/>
+          <Col xs={12} md={{span: 4, offset: 3}}>
+            <Card
+              style={{ border: "none" }}
+              onMouseEnter={() => setIsShown(!isShown)}
+            >
+              <Image src="/images/blog_home.png" alt="homeblogfeature" fluid />
               <Card.ImgOverlay>
                 <Card.Title className="blogHeader">Home</Card.Title>
               </Card.ImgOverlay>
@@ -42,7 +44,7 @@ const PersonalWriting = () => {
             </div>
           </Col>
 
-          <Col md={4}>
+          <Col xs={12} md={4}>
             <Card className="text-white rounded-0" style={{ border: "none" }}>
               <Image
                 className="img-port"
@@ -63,26 +65,24 @@ const PersonalWriting = () => {
               </ul>
             </div>
           </Col>
-
-          <Col md={4}>
-            <Card className="text-white rounded-0" style={{ border: "none"}}>
-              <Image src="/images/blog_3.png" alt="Card image" fluid/>
-              <Card.ImgOverlay className="blogImgCards">
-              <Card.Title className="blogHeader">
-                  Wisdom & Womanhood
-                </Card.Title>
-              </Card.ImgOverlay>
-              
-            </Card>
-            <div className="blog-list">
-              <ul>
-                <li>Discomfort and Peace of Being, Dec 2020</li>
-                <li>Inspire Series: Showing the Grace to Dream, Sept 2021</li>
-                <li>When Comparison Leads to Envy, June 2020</li>
-                <li>Before You Follow That Trend, June 2019</li>
-              </ul>
-            </div>
-          </Col>
+        </Row>
+        <Row>
+        <Col xs={12} md={{span: 4, offset: 3}}>
+          <Card className="text-white rounded-0" style={{ border: "none" }}>
+            <Image src="/images/blog_3.png" alt="Card image" fluid />
+            <Card.ImgOverlay className="blogImgCards">
+              <Card.Title className="blogHeader">Wisdom & Womanhood</Card.Title>
+            </Card.ImgOverlay>
+          </Card>
+          <div className="blog-list">
+            <ul>
+              <li>Discomfort and Peace of Being, Dec 2020</li>
+              <li>Inspire Series: Showing the Grace to Dream, Sept 2021</li>
+              <li>When Comparison Leads to Envy, June 2020</li>
+              <li>Before You Follow That Trend, June 2019</li>
+            </ul>
+          </div>
+        </Col>
         </Row>
       </Container>
     </div>
