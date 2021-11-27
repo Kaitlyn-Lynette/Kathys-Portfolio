1cbaf8aea0fcc82e3e2d210bd4885c4eb651561a
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-11-27 16:38:13 -0500

    Selected writing work

--- src/pages/SelectWriting.js
+++ src/pages/SelectWriting.js
@@ -82,7 +82,7 @@ const SelectWriting = () => {
                   I am writing a novel! It’s a coming-of-age story of a high
                   school senior leaving her comfortable home in North Carolina
                   and moving to NYC for college. 
-                  <p style={{fontSize: "20px", paddingTop: "40pxpx"}}> Are you an agent looking for a
+                  <p style={{fontSize: "22px", paddingTop: "40px"}}> Are you an agent looking for a
                   new author? Let’s get in touch!</p>
                 </Card.Text>
               </Card.Body>
