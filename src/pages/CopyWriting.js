67fbe1cf9651c57ddcae55f73fdb94c4be5ec83c
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-11-29 11:35:20 -0500

    Added image for copywrite and linked to Blend

--- src/pages/CopyWriting.js
+++ src/pages/CopyWriting.js
@@ -1,37 +1,60 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 const CopyWriting = () => {
   return (
-    <div
-      id="copywriting"   
-    >
+    <div id="copywriting">
       <Container>
         <Row>
-          <Col style={{ paddingTop: "80px", paddingBottom: "30px", textAlign: "center"}} md={12}>
+          <Col
+            style={{
+              paddingTop: "80px",
+              paddingBottom: "30px",
+              textAlign: "center",
+            }}
+            md={12}
+          >
             <h1>Copywriting </h1>
           </Col>
-          <Col md={{span: 8, offset: 2}} style={{ textAlign: "center"}}>
+          <Col md={{ span: 8, offset: 2 }} style={{ textAlign: "center" }}>
             <p>
               I have created, as well as followed, the brand voice for
               businesses and helped them share their stories on their websites.
             </p>
           </Col>
         </Row>
-        <Row style={{ paddingTop: "30px", paddingBottom: "80px", textAlign: "center"}}>
+        <Row
+          style={{
+            paddingTop: "30px",
+            paddingBottom: "80px",
+            textAlign: "center",
+          }}
+        >
           <Col xs={12} md={4}>
-            <Image src="/images/dummy_pic.jpeg" fluid></Image>
+            <Card className="copywriting-card h-100">
+              <a href="https://www.blendbayridge.com/" target="blank">
+                <Image src="/images/copywrite_blend.jpg" fluid></Image>
+              </a>
+            </Card>
           </Col>
           <Col xs={12} md={4}>
-            <Image src="/images/dummy_pic.jpeg" fluid></Image>
+            <Card className="copywriting-card h-100">
+              <a href="https://www.blendbayridge.com/">
+                <Image src="/images/copywrite_background.jpg" fluid></Image>
+              </a>
+            </Card>
           </Col>
-          
+
           <Col xs={12} md={4}>
-            <Image src="/images/dummy_pic.jpeg" fluid></Image>
+            <Card className="copywriting-card h-100">
+              <Image src="/images/copywrite_background.jpg" fluid></Image>
+            </Card>
           </Col>
         </Row>
       </Container>
