c958c6121b629e4f6819b6fbfb977723754b7446
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-11-30 12:53:40 -0500

    Added a form using formspark

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,65 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { useFormspark } from "@formspark/use-formspark";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+
+const FORMSPARK_FORM_ID = "3GPDUIaz";
 
 const Contact = () => {
+  const [submit, submitting] = useFormspark({
+    formId: FORMSPARK_FORM_ID,
+  });
+
+  const [message, setMessage] = useState("");
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await submit({ firstName, lastName, email, message });
+    alert("Form submitted");
+  };
+
   return (
-    <div id="contact">
-      <Container>
-        <Row>
-          <Col md={6} className="contact-form">
-            <h5>say hello</h5>
-            <h4>
-              If you have any questions or want to know more, please don't
-              hesitate to get in contact with me
-            </h4>
-          </Col>
-          <Col md={6}>
-            <Form>
-              <Row>
-                <Col>
-                  <Form.Control placeholder="First name" />
-                </Col>
-                <Col>
-                  <Form.Control placeholder="Last name" />
-                </Col>
-              </Row>
 
-              <Row>
-                <Col style={{ paddingTop: "20px" }}>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Col>
-              </Row>
+  <div id="contact">
+  <Container>
+    <Row>
+      <Col md={6} className="contact-form">
+        <h5>say hello</h5>
+        <h4>
+          If you have any questions or want to know more, please don't
+          hesitate to get in contact with me
+        </h4>
+      </Col>
+      <Col md={6}>
+        <Form onSubmit={onSubmit}>
+          <Row>
+            <Col>
+              <Form.Control 
+              placeholder="First name"
+              value={firstName} 
+              id="firstname" 
+              name="firstname"
+              onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Col>
+            <Col>
+              <Form.Control 
+              placeholder="Last name"
+              value={lastName}
+              id="lastname" 
+              name="lastname"
+              onChange={(e) => setLastName(e.target.value)}
+              />
+            </Col>
+          </Row>
 
-              <Row>
-                <Col style={{ paddingTop: "20px" }}>
-                  <Form.Control type="text" placeholder="Subject" />
-                </Col>
-              </Row>
+          <Row>
+            <Col style={{ paddingTop: "20px" }}>
+              <Form.Control 
+              type="email" 
+              placeholder="Enter email" 
+              id="email" 
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+          </Row>
 
-              <Row>
-                <Col>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                    style={{ paddingTop: "20px" }}
-                  >
-                    {/* <Form.Control placeholder="Enter your message" /> */}
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Enter your message"
-                      rows={3}
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
-export default Contact;
+          {/* <Row>
+            <Col style={{ paddingTop: "20px" }}>
+              <Form.Control type="text" placeholder="Subject" />
+            </Col>
+          </Row> */}
+
+          <Row>
+            <Col>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+                style={{ paddingTop: "20px" }}
+              >
+                <Form.Control
+                  as="textarea"
+                  placeholder="Enter your message"
+                  rows={3}
+                  value={message} 
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+        <Button type="submit" disabled={submitting}>
+        Contact Me!
+        </Button>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
+</div>
+  )
+}
+
+export default Contact
+
