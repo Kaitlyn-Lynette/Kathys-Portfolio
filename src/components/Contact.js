328202c5d699960b4e27846b35807952baf0b040
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-11-30 13:47:09 -0500

    Added a success message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,6 @@ import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-
 const FORMSPARK_FORM_ID = "3GPDUIaz";
 
 const Contact = () => {
@@ -16,95 +15,103 @@ const Contact = () => {
   });
 
   const [message, setMessage] = useState("");
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [email, setEmail] = useState("")
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
+  const [thankYou, setThankYou] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await submit({ firstName, lastName, email, message });
-    console.log("submitted!")
+    console.log("Submission starting", e);
+
+    const result = await submit({ firstName, lastName, email, message });
+    console.log("Submitting complete", result);
     setMessage("");
     setFirstName("");
     setLastName("");
     setEmail("");
+    setThankYou(`Your message has been sent successfully.`)
   };
 
   return (
+    <div id="contact">
+      <Container>
+        <Row>
+          <Col md={6} className="contact-form">
+            <h5>say hello</h5>
+            <h4>
+              If you have any questions or want to know more, please don't
+              hesitate to get in contact with me
+            </h4>
+          </Col>
+          <Col md={6}>
+            <Form onSubmit={onSubmit}>
+            {thankYou}
+              <Row>
+                <Col>
+                  <Form.Control
+                    placeholder="First name"
+                    value={firstName}
+                    id="firstname"
+                    name="firstname"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <Form.Control
+                    placeholder="Last name"
+                    value={lastName}
+                    id="lastname"
+                    name="lastname"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </Col>
+              </Row>
 
-  <div id="contact">
-  <Container>
-    <Row>
-      <Col md={6} className="contact-form">
-        <h5>say hello</h5>
-        <h4>
-          If you have any questions or want to know more, please don't
-          hesitate to get in contact with me
-        </h4>
-      </Col>
-      <Col md={6}>
-        <Form onSubmit={onSubmit}>
-          <Row>
-            <Col>
-              <Form.Control 
-              placeholder="First name"
-              value={firstName} 
-              id="firstname" 
-              name="firstname"
-              onChange={(e) => setFirstName(e.target.value)}
-              />
-            </Col>
-            <Col>
-              <Form.Control 
-              placeholder="Last name"
-              value={lastName}
-              id="lastname" 
-              name="lastname"
-              onChange={(e) => setLastName(e.target.value)}
-              />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col style={{ paddingTop: "20px" }}>
-              <Form.Control 
-              type="email" 
-              placeholder="Enter email" 
-              id="email" 
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-                style={{ paddingTop: "20px" }}
+              <Row>
+                <Col style={{ paddingTop: "20px" }}>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                    style={{ paddingTop: "20px" }}
+                  >
+                    <Form.Control
+                      as="textarea"
+                      placeholder="Enter your message"
+                      rows={3}
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Button
+                type="submit"
+                className="rwc-button"
+                disabled={submitting}
               >
-                <Form.Control
-                  as="textarea"
-                  placeholder="Enter your message"
-                  rows={3}
-                  value={message} 
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-        <Button type="submit" className="rwc-button" disabled={submitting}>
-        Contact Me!
-        </Button>
-        </Form>
-      </Col>
-    </Row>
-  </Container>
-</div>
-  )
-}
+                Contact Me!
+              </Button>
+              
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default Contact
-
+export default Contact;
