e585ae6e732e3277ec6bd5fe0ab8b3243b9b1bff
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-12-13 11:56:07 -0500

    Guest Writing mobile sytled

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,17 +11,17 @@ const Portfolio = () => {
     <div id="portfolio">
       <Container>
         <Row>
-        <Col style={{ paddingTop: "80px", paddingBottom: "30px", textAlign: "center"}} md={12}>
+        <Col className = "port-pagetitle" md={12}>
             <h1>Guest Writing</h1>
         </Col>
-        <Col style={{ paddingTop: "80px", paddingBottom: "30px", textAlign: "center" }} md={{span: 8, offset: 2}}>
+        <Col style={{ textAlign: "center" }} md={{span: 8, offset: 2}}>
             <p> It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference. </p>
         </Col>
         </Row>
 
-        <Row style={{marginLeft: "19px;"}}>
-          <Col md={{span: 8, offset: 1}}>
-          <Image src="/images/wholeness-for-the-lonely-heart.jpeg" style={{align: "center",width: "1000px", height: "600px"}}></Image>
+        <Row style={{marginLeft: "19px", paddingTop: "30px"}}>
+          <Col md={{span: 8, offset: 1}} sm={12}>
+          <Image className="guest-writing-photo"src="/images/wholeness-for-the-lonely-heart.jpeg"></Image>
           </Col>
         </Row>
           <Col  md={{span: 8, offset: 2}} style={{paddingTop: "60px", paddingBottom: "80px", textAlign: "center"}}>
