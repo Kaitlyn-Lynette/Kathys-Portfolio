d10bb87623a1aade46d9f3aa70eb387d0fffbbf0
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-12-15 10:06:09 -0500

    Changed the padding under the column of novel writing

--- src/pages/NovelWriting.js
+++ src/pages/NovelWriting.js
@@ -10,7 +10,7 @@ const NovelWriting = () => {
     <div id="novelwriting">
       <Container>
         <Row >
-          <Col style={{paddingBottom: "80px"}} className="port-pagetitle"
+          <Col style={{paddingBottom: "30px"}} className="port-pagetitle"
             md={12}
           >
             <h1>Novel Writing</h1>
