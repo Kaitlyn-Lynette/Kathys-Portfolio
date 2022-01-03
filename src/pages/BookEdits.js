eb11e3ac31e77ec3f433d4243db0ab470e292257
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2022-01-03 12:07:12 -0500

    Removed mentions of Matter work as requested by client

--- src/pages/BookEdits.js
+++ src/pages/BookEdits.js
@@ -17,53 +17,40 @@ const BookEdits = () => {
           <Col className="port-subtitle" md={6}>
             <h3>Edits & Proofs:</h3>
           </Col>
-          <Col md={6} sm={8} style={{textAlign: "justify"}}>
+          <Col md={6} sm={8} style={{ textAlign: "justify" }}>
             <p>
               I’ve been in on some pretty stellar editing and proofreading
               manuscripts and copy for some of my network’s most ambitious
               authors. I helped them get their stories and ideas across and made
-              sure they sounded good doing it.
+              sure they sounded good doing it. I especially would like to
+              highlight my work with C Lenoir Publishing.
             </p>
           </Col>
         </Row>
 
         <Row>
           <Col md={{ span: 6, offset: 6 }} className="edits-proofs">
-            <p>
-              Contributing Editor, Mattera Ministries International:{" "}
-              <a href="https://josephmattera.org/the-jesus-principles/">
-                The Jesus Principles,
-              </a>{" "}
-              <a href="https://josephmattera.org/poisonous-power/">
-                Poisonous Power
-              </a>
-              ,{" "}
-              <a href="https://josephmattera.org/the-divided-gospel/">
-                The Divided Gospel
-              </a>{" "}
-            </p>
+            <ul>
+              <li>
+                <a href="https://feliciakirksey.com/">
+                  {" "}
+                  From Darkness To Destiny
+                </a>{" "}
+              </li>
+              <li>
+                <a href="https://www.amazon.com/morning-Jazzmine-Bustamante/dp/1737483815/ref=sr_1_2?keywords=jazzmine+bustamante&qid=1638636055&sr=8-2">
+                  In the Morning.
+                </a>
+              </li>
+            </ul>
           </Col>
         </Row>
-
-        <Row>
-        <Col md={{ span: 6, offset: 6 }} className="edits-proofs">
-          <p>Editor, C Lenoir Publishing:{" "}
-          <a href="https://feliciakirksey.com/">
-            From Darkness To Destiny
-          </a> and{" "}
-          <a href="https://www.amazon.com/morning-Jazzmine-Bustamante/dp/1737483815/ref=sr_1_2?keywords=jazzmine+bustamante&qid=1638636055&sr=8-2">
-            In the Morning.
-          </a>
-          </p>
-        </Col>
-        </Row>
-
 
         <Row style={{ paddingTop: "60px", paddingBottom: "80px" }}>
-          <Col className="port-subtitle" md={6} >
+          <Col className="port-subtitle" md={6}>
             <h3>Copywriting</h3>
           </Col>
-          <Col md={6} style={{textAlign: "justify"}}>
+          <Col md={6} style={{ textAlign: "justify" }}>
             <p>
               It’s one thing to hustle, it’s another to make sure you’re staying
               consistent with values, voice, and brand identity. Curating copy
