30fa31277b20e4fb36e26fd0f23dbece031474b7
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2022-01-03 13:32:37 -0500

    Added book icon

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
       <Services />
       <Contact />
       <Footer />
-      
     </div>
   );
 }
--- src/pages/BookEdits.js
+++ src/pages/BookEdits.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../App.css";
 
 const BookEdits = () => {
@@ -17,7 +18,7 @@ const BookEdits = () => {
           <Col className="port-subtitle" md={6}>
             <h3>Edits & Proofs:</h3>
           </Col>
-          <Col md={6} sm={8} style={{ textAlign: "justify" }}>
+          <Col md={6} sm={8} className="edits-proofs" style={{ textAlign: "justify" }}>
             <p>
               I’ve been in on some pretty stellar editing and proofreading
               manuscripts and copy for some of my network’s most ambitious
@@ -25,22 +26,19 @@ const BookEdits = () => {
               sure they sounded good doing it. I especially would like to
               highlight my work with C Lenoir Publishing.
             </p>
-          </Col>
-        </Row>
 
-        <Row>
-          <Col md={{ span: 6, offset: 6 }} className="edits-proofs">
-            <ul>
+          <ul className="fa-ul">
               <li>
+              <FontAwesomeIcon icon={["fas", "book-open"]} size="sm" pull="right" listItem/>
                 <a href="https://feliciakirksey.com/">
-                  {" "}
                   From Darkness To Destiny
-                </a>{" "}
-              </li>
-              <li>
-                <a href="https://www.amazon.com/morning-Jazzmine-Bustamante/dp/1737483815/ref=sr_1_2?keywords=jazzmine+bustamante&qid=1638636055&sr=8-2">
-                  In the Morning.
                 </a>
+              </li>
+              <li> 
+              <FontAwesomeIcon icon={["fas", "book-open"]} size="sm" pull="right" listItem/>
+              <a href="https://www.amazon.com/morning-Jazzmine-Bustamante/dp/1737483815/ref=sr_1_2?keywords=jazzmine+bustamante&qid=1638636055&sr=8-2">
+                In the Morning
+              </a> 
               </li>
             </ul>
           </Col>
